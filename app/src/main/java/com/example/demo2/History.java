c562f87fd2b06c2940253ba9ff54a84d6088281f
MaxwellHUBIV <65359664+MaxwellHUBIV@users.noreply.github.com>
2020-12-11 11:58:27 +0900

    History and Settings update

--- app/src/main/java/com/example/demo2/History.java
+++ app/src/main/java/com/example/demo2/History.java
@@ -2,13 +2,29 @@ package com.example.demo2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class History extends AppCompatActivity {
+
+    ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        imageButton = (ImageButton) findViewById(R.id.button_back);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(History.this, StartScreen.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/demo2/Settings.java
+++ app/src/main/java/com/example/demo2/Settings.java
@@ -2,13 +2,39 @@ package com.example.demo2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Settings extends AppCompatActivity {
+
+    ImageButton imageButton2;
+    Button exitbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        imageButton2 = (ImageButton) findViewById(R.id.buttonback2);
+        exitbtn = findViewById(R.id.exitbtn);
+
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Settings.this, StartScreen.class);
+                startActivity(intent);
+            }
+        });
+
+        exitbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Settings.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
